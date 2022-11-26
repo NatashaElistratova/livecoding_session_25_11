59d3d7b16a8e8175de711e9f1ef4968690a570a1
natalia.yelistratova <natalia.yelistratova@gmail.com>
2022-11-26 15:34:43 +0300

    Set radio and checkboxes value

--- index.js
+++ index.js
@@ -3,20 +3,37 @@ const getFormData = () => {
     const lname = document.querySelector("#lname").value;
     const fruit = document.querySelector("select").value;
     const language = document.querySelector(
-        'input[name="fav_language"]:checked'
+        'input[name="language"]:checked'
     ).value;
     const vehicleValues = Array.from(
         document.querySelectorAll('input[name="vehicle"]:checked')
     );
-    const vehicles = vehicleValues.map((item) => item.value);
-    const data = { fname, lname, fruit, language, vehicles };
+    const vehicle = vehicleValues.map((item) => item.value);
+    const data = { fname, lname, fruit, language, vehicle };
 
     return data;
 };
 
 const setFormData = (data) => {
     for (const key in data) {
-        document.getElementById(`${key}`).value = data[key];
+        const singleInput = document.getElementById(`${key}`);
+        // если это одиночный инпут, присваиваем ему value 
+        if (singleInput) {
+            // через ключ в квадратных скобках получаем значение свойства объекта (value)
+            singleInput.value = data[key];
+        } else {
+            // если это не одиночный инпут (радио или чекбокс), находим все по name 
+            const multipleInput = document.querySelectorAll(`input[name=${key}]`);
+            
+            // и бежим по списку найденных элементов циклом 
+            multipleInput.forEach(item => {
+                // если значение свойства объекта равно value инпута (радио) или включает его (чекбоксы),
+                // устанавливаем атрибут checked этого инпута true 
+                if (item.value === data[key] || data[key].includes(item.value)) {
+                   item.checked = true; 
+                }
+            })
+        }
     }
 };
 
@@ -34,5 +51,3 @@ const getDataFromLS = (key) => {
 const formData = JSON.parse(getDataFromLS("formData"));
 
 setFormData(formData);
-
-console.log(formData);
